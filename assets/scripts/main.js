9e7e67e774163840f59dbb70528a591b86f5b880
ThiagoAguirre <thiago.aguirre08@outlook.com>
2024-10-27 01:09:19 -0300

    fix: login href and create home.html and test.html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,22 @@
-function password() {
+function getLogin() {
     var login = document.getElementById('login').value;
     var password = document.getElementById('password').value;
+    const validateLogin = verificarLogin(login, password);
 
-    const redirect = verificarLogin(login, password);
-
-    if (redirect) {
-        alert(`Welcome ${login} a ORDEM`);
-        location.href = redirect;
+    if (validateLogin) {
+        alert('Welcome ${login} a ORDEM ${password}');
+        location.href = "./assets/templates/home.html";
     } else {
-        alert('user or password incorrect');
-
+        alert('User or password incorrect');
     }
+}
+
+function verificarLogin(login, password) {
+    const users = {
+        "123": "123",  
+        "222": "222",
+        "arrozdamamae": "9y4s2da7"
+    };
+
+    return users[login] === password;
 }
